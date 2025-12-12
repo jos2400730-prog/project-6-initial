9e4e90baef4a17c243b7d669b004496613eccc39
Spyro1001 <raymondhedgecock@gmail.com>
2025-12-11 18:42:09 -0700

    update to aggregator

---
+++ aggregator/src/main/java/com/example/aggregator/client/AggregatorRestClient.java
@@ -0,0 +1,76 @@
+package com.example.aggregator.client;
+
+import com.example.aggregator.model.Entry;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AggregatorRestClient {
+
+    private RestTemplate restTemplate;
+
+    public AggregatorRestClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Entry getDefinitionFor(String word) {
+
+        String uri = "http://localhost:9091/getWord/" + word;
+
+        Entry result = restTemplate.getForObject(uri, Entry.class);
+
+        return result;
+    }
+
+
+    public List<Entry> getWordsStartingWith(String chars) {
+
+        String uri = "http://localhost:9091/getWordsStartingWith/" + chars;
+
+        ResponseEntity<Entry[]> result = restTemplate.getForEntity(uri, Entry[].class);
+        Entry[] body = result.getBody();
+
+        return Arrays.stream(body)
+                .collect(Collectors.toList());
+    }
+
+    public List<Entry> getWordsThatContainConsecutiveLetters() {
+
+        String uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9091") // replace with injected baseUrl
+                .path("/getWordsThatContainConsecutiveLetters")
+                .build()
+                .encode()
+                .toUri()
+                .toString();
+
+        ResponseEntity<Entry[]> result = restTemplate.getForEntity(uri, Entry[].class);
+        Entry[] body = result.getBody();
+
+        return Arrays.stream(body)
+                .collect(Collectors.toList());
+    }
+
+    public List<Entry> getWordsThatContain(String chars) {
+
+        String uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9091") // replace with injected baseUrl
+                .path("/getWordsThatContain/" + chars)
+                .build()
+                .encode()
+                .toUri()
+                .toString();
+
+        ResponseEntity<Entry[]> result = restTemplate.getForEntity(uri, Entry[].class);
+        Entry[] body = result.getBody();
+
+        return Arrays.stream(body)
+                .collect(Collectors.toList());
+    }
+}
---
+++ aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
@@ -0,0 +1,67 @@
+package com.example.aggregator.controller;
+
+import com.example.aggregator.model.Entry;
+import com.example.aggregator.service.AggregatorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class AggregatorController {
+
+    private static final Logger log = LoggerFactory.getLogger(AggregatorController.class);
+
+    private AggregatorService aggregatorService;
+
+    public AggregatorController(AggregatorService aggregatorService) {
+        this.aggregatorService = aggregatorService;
+    }
+
+    @GetMapping("/")
+    public List<Entry> helloWorld() {
+        List<Entry> entries = new ArrayList<>();
+        entries.add(aggregatorService.getDefinitionFor("hello"));
+        entries.add(aggregatorService.getDefinitionFor("world"));
+        return entries;
+    }
+
+    @GetMapping("/getDefinitionFor/{word}")
+    public Entry getDefinitionFor(@PathVariable String word) {
+        return aggregatorService.getDefinitionFor(word);
+    }
+
+    @GetMapping("/getWordsThatContainSuccessiveLettersAndStarsWith/{chars}")
+    public List<Entry> getWordsThatContainSuccessiveLettersAndStartsWith(@PathVariable String chars) {
+
+        return aggregatorService.getWordsThatContainSuccessiveLettersAndStartsWith(chars);
+    }
+
+    @GetMapping("/getWordsThatContain/{chars}")
+    public List<Entry> getWordsThatContain(String chars) {
+
+        StopWatch sw = new StopWatch();
+        sw.start();
+        List<Entry> entries = aggregatorService.getWordsThatContain(chars);
+        sw.stop();
+
+        long nanoSeconds = sw.getTotalTimeNanos();
+        String message = new StringBuilder().append("Retrieved entries for words containing [")
+                                            .append(chars)
+                                            .append("], ")
+                                            .append(" containing ")
+                                            .append(entries.size())
+                                            .append(" entries in ")
+                                            .append(nanoSeconds / 1000000.0)
+                                            .append("ms")
+                                            .toString();
+        log.info(message);
+
+        return entries;
+    }
+}
